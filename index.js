6d94ddf51ef9a0f009e033cfee4500a744f0126e
sakkuru <sakkuru0323@gmail.com>
2018-05-21 12:51:10 +0900

    rotate LED

--- index.js
+++ index.js
@@ -14,22 +14,20 @@ app.post('/banana', (req, res) => {
     console.log('banana!!!');
     const O = [0, 0, 0];
     const X = [255, 255, 0];
-    const R = [255, 150, 0];
-    const Y = [150, 80, 50];
 
-    const bananaLED = [
+    const smile = [
         O, O, X, X, X, X, O, O,
         O, X, O, O, O, O, X, O,
-        X, O, O, O, O, O, O, X,
-        X, O, X, O, O, X, O, X,
-        X, O, O, O, O, O, O, X,
-        X, O, X, X, X, X, O, X,
+        X, O, X, O, X, O, O, X,
+        X, O, X, O, O, O, O, X,
+        X, O, X, O, O, O, O, X,
+        X, O, X, O, X, O, O, X,
         O, X, O, O, O, O, X, O,
         O, O, X, X, X, X, O, O,
     ];
 
     matrix.setPixels(bananaLED);
-    res.send(200, "Yes!!", () => {});
+    res.send(200, "banana!!", () => {});
 });
 
 app.post('/apple', (req, res) => {
@@ -40,13 +38,14 @@ app.post('/apple', (req, res) => {
     const smile = [
         O, O, X, X, X, X, O, O,
         O, X, O, O, O, O, X, O,
-        X, O, O, O, O, O, O, X,
-        X, O, X, O, O, X, O, X,
-        X, O, O, O, O, O, O, X,
-        X, O, X, X, X, X, O, X,
+        X, O, X, O, X, O, O, X,
+        X, O, X, O, O, O, O, X,
+        X, O, X, O, O, O, O, X,
+        X, O, X, O, X, O, O, X,
         O, X, O, O, O, O, X, O,
         O, O, X, X, X, X, O, O,
     ];
+
     matrix.setPixels(smile);
     res.send(200, "Yes!!", () => {});
 });
@@ -59,17 +58,17 @@ app.post('/pineapple', (req, res) => {
     const Y = [255, 120, 10];
     const G = [0, 255, 0];
 
-    const cross = [
-        O, O, O, G, O, G, O, O,
-        O, O, G, G, G, G, G, O,
-        O, G, O, G, G, G, O, G,
-        O, O, O, X, X, X, O, O,
-        O, O, X, Y, X, Y, X, O,
-        O, O, Y, X, Y, X, Y, O,
-        O, O, X, Y, X, Y, X, O,
-        O, O, O, X, X, X, O, O,
+    const pineappleLED = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, G, O, O,
+        O, X, Y, X, O, O, G, O,
+        X, Y, X, Y, X, G, G, G,
+        X, X, Y, X, X, G, G, O,
+        X, Y, X, Y, X, G, G, G,
+        O, X, Y, X, O, O, G, O,
+        O, O, O, O, O, G, O, O,
     ];
 
-    matrix.setPixels(cross);
+    matrix.setPixels(pineappleLED);
     res.send(200, "pineapple!!", () => {});
 });
